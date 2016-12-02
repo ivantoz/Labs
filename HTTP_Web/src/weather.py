88aff24a84ce3b8d85ec0b888b03d37ede452c1e
ivantoz <ivan@bardo.com>
2016-12-03 02:54:41 +0300

    amend commit

--- HTTP_Web/src/weather.py
+++ HTTP_Web/src/weather.py
@@ -1,13 +1,59 @@
 import json
 import requests
+import datetime
+from HTTP_Web.src.utilities import degrees_to_direction
+from HTTP_Web.src.utilities import bcolors
 
-print ("\n\nWelcome to this simple Commandline Weather App \n", "-"*46)
-city = input("Enter the name of the City you want to query weather information: ")
+underline = '-'*55
+print(bcolors.HEADER,'\t', underline, bcolors.ENDC)
+heading ='\n\t\tWelcome to this simple Commandline Weather App \n '
+print(bcolors.HEADER, heading.upper(), bcolors.ENDC)
+print(bcolors.HEADER,'\t', underline, bcolors.ENDC)
+subheading = '\t\t\t\tThe ultimate weather app'
+print(bcolors.OKGREEN, subheading,bcolors.ENDC)
+
+print('\t', bcolors.BOLD)
+city = input("Enter the name of place to search: ")
+print(bcolors.ENDC)
+city = city.strip()
 response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=857013fd87ce20b5e5305b3f35bab216")
-print ("\n", response.status_code, response.reason, "- The Connection was successful!\n")
+
+if response.status_code ==200:
+    msg = '- The Connection was successful!\n'
+    print(bcolors.OKGREEN,response.status_code, response.reason, msg, bcolors.ENDC)
 
 
 
 data = json.loads(response.text)
 
-print ("ID:", data['id'], "\nCity:", data['name'], "\nCountry Code:", data['sys']['country'], "\nCoordinates: ", str(data['coord']['lon']) + ", " + str(data['coord']['lat']), "\nDate:", data['dt'], "\nDescription:", data['weather'][0]['description'], "\nHumidity :", data['main']['humidity'], "\nPressure :", data['main']['pressure'], "\nWind Speed :", data['wind']['speed'], "\nWind Deg :", data['wind']['deg'], "\nTemperature:", str(data['main']['temp'] - 273) + u"\N{DEGREE SIGN}C")
+description = data['weather'][0]['description'].capitalize()
+dt = datetime.datetime.fromtimestamp(data['dt'])
+date_obtained = dt.strftime("%A, %d %B %Y %I:%M%p")
+temperatue = str(data['main']['temp'] - 273) + u"\N{DEGREE SIGN}C"
+pressure = data['main']['pressure']
+humidity = data['main']['humidity']
+wind_direction = degrees_to_direction(data['wind']['deg'])
+wind_speed = data['wind']['speed']
+sr = datetime.datetime.fromtimestamp(data['sys']['sunrise'])
+sunrise = sr.strftime("%I:%M%p")
+st = datetime.datetime.fromtimestamp(data['sys']['sunset'])
+sunset = st.strftime("%I:%M%p")
+country_code = data['sys']['country']
+cordinates = str(data['coord']['lon']) + ", " + str(data['coord']['lat'])
+
+
+print("\n")
+print('\t', bcolors.HEADER, 'Weather  in ', city.capitalize(), ',', country_code, bcolors.ENDC, '\n')
+print('\t', bcolors.OKBLUE, 'Description\t:', bcolors.ENDC, bcolors.OKGREEN, description, bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Date obtained\t:', bcolors.ENDC, bcolors.OKGREEN, date_obtained, bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Temperature\t:', bcolors.ENDC, bcolors.OKGREEN, temperatue, bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Pressure\t\t:', bcolors.ENDC, bcolors.OKGREEN, pressure, ' hpa', bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Humidity\t\t:', bcolors.ENDC, bcolors.OKGREEN, humidity, '%', bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Wind Direction:', bcolors.ENDC, bcolors.OKGREEN, wind_direction, bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Wind Speed\t:', bcolors.ENDC, bcolors.OKGREEN, wind_speed, ' m/s', bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Sunrise\t\t:', bcolors.ENDC, bcolors.OKGREEN, sunrise, bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Sunset\t\t:', bcolors.ENDC, bcolors.OKGREEN, sunset, bcolors.ENDC)
+print('\t', bcolors.OKBLUE, 'Cordinates\t:', bcolors.ENDC, bcolors.OKGREEN, cordinates, bcolors.ENDC)
+
+print('\n', bcolors.BOLD, "Powered by http://openweathermap.org/", bcolors.ENDC)
+
